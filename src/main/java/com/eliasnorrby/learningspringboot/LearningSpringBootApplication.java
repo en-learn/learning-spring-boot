e14bab2697f95bb95799c8726f969b93fa086d6d
Elias Norrby <elias.norrby@gmail.com>
2020-10-18 19:54:07 +0200

    Rename package

--- src/main/java/com/eliasnorrby/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/com/eliasnorrby/learningspringboot/LearningSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.eliasnorrby.learningspringboot.learningspringboot;
+package com.eliasnorrby.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningSpringBootApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LearningSpringBootApplication.class, args);
+  }
 }
--- src/test/java/com/eliasnorrby/learningspringboot/LearningSpringBootApplicationTests.java
+++ src/test/java/com/eliasnorrby/learningspringboot/LearningSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eliasnorrby.learningspringboot.learningspringboot;
+package com.eliasnorrby.learningspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
