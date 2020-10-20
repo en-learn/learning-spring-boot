9c7938b758664c1d5da3b9a3a82d82678bd769f9
Elias Norrby <elias.norrby@gmail.com>
2020-10-20 23:20:20 +0200

    L15: Testing with a real MongoDB database

---
+++ social-network/src/test/java/com/eliasnorrby/socialnetwork/LiveImageRepositoryTests.java
@@ -0,0 +1,67 @@
+package com.eliasnorrby.socialnetwork;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoOperations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+public class LiveImageRepositoryTests {
+
+  @Autowired ImageRepository repository;
+
+  @Autowired MongoOperations operations;
+
+  @BeforeEach
+  public void setUp() {
+    operations.dropCollection(Image.class);
+
+    operations.insert(new Image("1", "learning-spring-boot-cover.jpg"));
+    operations.insert(new Image("2", "learning-spring-boot-2nd-edition-cover.jpg"));
+    operations.insert(new Image("3", "bazinga.png"));
+
+    operations.findAll(Image.class).forEach(System.out::println);
+  }
+
+  @Test
+  public void findAllShouldWork() {
+    Flux<Image> images = repository.findAll();
+    StepVerifier.create(images)
+      .recordWith(ArrayList::new)
+      .expectNextCount(3)
+      .consumeRecordedWith(
+        results -> {
+          assertThat(results).hasSize(3);
+          assertThat(results)
+            .extracting(Image::getName)
+            .contains(
+              "learning-spring-boot-cover.jpg",
+              "learning-spring-boot-2nd-edition-cover.jpg",
+              "bazinga.png");
+        })
+      .expectComplete()
+      .verify();
+  }
+
+  @Test
+  public void findByNameShouldWork() {
+    Mono<Image> image = repository.findByName("bazinga.png");
+    StepVerifier.create(image)
+      .expectNextMatches(
+        results -> {
+          assertThat(results.getName()).isEqualTo("bazinga.png");
+          assertThat(results.getId()).isEqualTo("3");
+          return true;
+        })
+      .expectComplete()
+      .verify();
+  }
+}
