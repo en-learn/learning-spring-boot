1022e9e3b0fa51d4a3aec7a3a60106afeaf10de6
Elias Norrby <elias.norrby@gmail.com>
2020-10-23 00:03:00 +0200

    L17: Writing a custom health check
    This seems to work a little differently nowadays. /health only has
    'status', nothing more.

---
+++ social-network/src/test/java/com/eliasnorrby/socialnetwork/EndToEndTests.java
@@ -0,0 +1,12 @@
+package com.eliasnorrby.socialnetwork;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EndToEndTests {
+
+  @LocalServerPort
+  int port;
+}
