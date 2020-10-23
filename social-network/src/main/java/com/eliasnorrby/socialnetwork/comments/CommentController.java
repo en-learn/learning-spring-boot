8e4554a0dc4a5da17042772df0d180c2e9cbe063
Elias Norrby <elias.norrby@gmail.com>
2020-10-23 23:31:58 +0200

    L23: Adding customized metrics to track message flow

--- social-network/src/main/java/com/eliasnorrby/socialnetwork/comments/CommentController.java
+++ social-network/src/main/java/com/eliasnorrby/socialnetwork/comments/CommentController.java
@@ -1,6 +1,7 @@
 package com.eliasnorrby.socialnetwork.comments;
 
 import com.eliasnorrby.socialnetwork.images.Comment;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Controller;
@@ -13,15 +14,25 @@ public class CommentController {
 
   private final RabbitTemplate rabbitTemplate;
 
+  private final MeterRegistry meterRegistry;
+
   @PostMapping("/comments")
   public Mono<String> addComment(Mono<Comment> newComment) {
-    return newComment.flatMap(
-        comment ->
-            Mono.fromRunnable(
-                    () ->
-                        rabbitTemplate.convertAndSend(
-                            "learning-spring-boot", "comments.new", comment)))
-                .log("commentService-publish")
-                .then(Mono.just("redirect:/"));
+    return newComment
+        .flatMap(
+            comment ->
+                Mono.fromRunnable(
+                        () ->
+                            rabbitTemplate.convertAndSend(
+                                "learning-spring-boot", "comments.new", comment))
+                    .then(Mono.just(comment)))
+        .log("commentService-publish")
+        .flatMap(
+            comment -> {
+              meterRegistry
+                  .counter("comments.produced", "imageId", comment.getImageId())
+                  .increment();
+              return Mono.just("redirect:/");
+            });
   }
 }
--- social-network/src/main/java/com/eliasnorrby/socialnetwork/comments/CommentService.java
+++ social-network/src/main/java/com/eliasnorrby/socialnetwork/comments/CommentService.java
@@ -1,5 +1,6 @@
 package com.eliasnorrby.socialnetwork.comments;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -16,17 +17,23 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
   private final CommentWriterRepository repository;
+  private final MeterRegistry meterRegistry;
 
-  @RabbitListener(bindings = @QueueBinding(
-    value = @Queue,
-    exchange = @Exchange(value = "learning-spring-boot"),
-    key = "comments.new"
-  ))
+  @RabbitListener(
+      bindings =
+          @QueueBinding(
+              value = @Queue,
+              exchange = @Exchange(value = "learning-spring-boot"),
+              key = "comments.new"))
   public void save(Comment newComment) {
     repository
-      .save(newComment)
-      .log("commentService-save")
-      .subscribe();
+        .save(newComment)
+        .log("commentService-save")
+        .subscribe(
+            comment ->
+                meterRegistry
+                    .counter("comments.consumed", "imageId", comment.getImageId())
+                    .increment());
   }
 
   @Bean
@@ -38,5 +45,4 @@ public class CommentService {
   CommandLineRunner setUpComments(MongoOperations operations) {
     return args -> operations.dropCollection(Comment.class);
   }
-
 }
